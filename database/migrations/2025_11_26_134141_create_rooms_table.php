20a191cfb22171e43f51fd2ada9c96346abbbd42
RasyaDevansyah <rasya.devan21@gmail.com>
2025-11-26 20:52:29 +0700

    Room model & migration

--- database/migrations/2025_11_26_134141_create_rooms_table.php
+++ database/migrations/2025_11_26_134141_create_rooms_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,24 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
+
+            // hostID: Foreign Key to User
+            $table->foreignId('host_id')->constrained('users')->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('description');
+
+            // uniqueToken: Should be unique
+            $table->string('unique_token')->unique();
+
+            // Date fields
+            $table->date('start_date');
+            $table->date('end_date');
+
+            // Boolean fields
+            $table->boolean('is_revealed')->default(false);
+            $table->boolean('is_closed')->default(false);
+
             $table->timestamps();
         });
     }
