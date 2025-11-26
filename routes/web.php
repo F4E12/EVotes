310af508a21696b0007705e0bc21b20dee96530e
RasyaDevansyah <rasya.devan21@gmail.com>
2025-11-26 21:36:38 +0700

    Added more view files

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,25 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('pages.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/history', function () {
+    return view('pages.history');
+})->middleware(['auth', 'verified'])->name('history');
+
+Route::get('/host-a-room', function () {
+    return view('pages.host-a-room');
+})->middleware(['auth', 'verified'])->name('host-a-room');
+
+Route::get('/join-a-room', function () {
+    return view('pages.join-a-room');
+})->middleware(['auth', 'verified'])->name('join-a-room');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
