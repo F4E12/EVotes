70e29b9c9a2e23e3b579907821b389c0b3667a74
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-11-29 14:37:47 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    server: {
-        host: true,
-        port: 5173,
-        strictPort: true,
-    },
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: true,
+            }),
+        ],
+        server: {
+            host: env.APP_ENV === "local" ? "127.0.0.1" : true,
+
+            port: 5173,
+            strictPort: true,
+
+            hmr: {
+                host: env.APP_ENV === "local" ? "127.0.0.1" : undefined,
+            },
+        },
+    };
 });
